ddc8b3e60a8343acb2699f4e9e4fe314174f4578
June Trinh <thanchitr@outlook.com>
2025-05-18 01:08:23 +1000

    feat(readonly-app-db-context): explicitly block write, enqueue modification and return menaningful exception to ensure FAIL FAST and FAIL LOUD

---
+++ src/Modules/Crud/Crud.Application/Interface/System/Reader.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Infrastructure.Redis.Core.Read;
+using Shared.Infrastructure.Redis.Interface.Core;
+
+namespace Crud.Application.Interface.System
+{
+    public class Reader<KeyDTO, ValueDTO> 
+        where KeyDTO : IRedisSerialise
+    {
+        private RedisReader<KeyDTO, ValueDTO> _cache { get; set; }
+
+    }
+}
---
+++ src/Shared/Shared.API/Shared.Infrastructure/Data/ReadOnlyAppSqlDbContext.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Shared.Infrastructure.Data
+{
+    public class ReadOnlyAppSqlDbContext : AppSqlDbContext
+    {
+
+        public ReadOnlyAppSqlDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking); // we dont need tracking, since it is all read
+        }
+
+        public override int SaveChanges()
+        {
+            throw new InvalidOperationException("Read-only context: SaveChanges is disabled.");
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new InvalidOperationException("Read-only context: SaveChangesAsync is disabled.");
+        }
+
+        public override EntityEntry<TEntity> Add<TEntity>(TEntity entity)
+        {
+            throw new InvalidOperationException("Read-only context: Add is disabled.");
+        }
+
+        public override EntityEntry<TEntity> Update<TEntity>(TEntity entity)
+        {
+            throw new InvalidOperationException("Read-only context: Update is disabled.");
+        }
+
+        public override EntityEntry<TEntity> Remove<TEntity>(TEntity entity)
+        {
+            throw new InvalidOperationException("Read-only context: Remove is disabled.");
+        }
+
+        public override void AddRange(params object[] entities)
+        {
+            throw new InvalidOperationException("Read-only context: AddRange is disabled.");
+        }
+    }
+}
